d0f1a5e03ec1f598839a8b08c158ea67ec2be2f4
Ming Bian <127263871+Ming-B@users.noreply.github.com>
2025-03-23 08:11:31 -0400

    Initial Commit

---
+++ Quizzo/QuizzoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzoApp.swift
+//  Quizzo
+//
+//  Created by Ming Bian on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct QuizzoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizzoTests/QuizzoTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuizzoTests.swift
+//  QuizzoTests
+//
+//  Created by Ming Bian on 3/23/25.
+//
+
+import Testing
+
+struct QuizzoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
